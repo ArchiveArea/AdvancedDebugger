6444ee685effda5908c0a45bbb5034452a1e28cb
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-08-31 12:57:23 +0700

    `BlockGrowEvent`

--- src/NhanAZ/AdvancedDebuger/EventListener.php
+++ src/NhanAZ/AdvancedDebuger/EventListener.php
@@ -7,6 +7,7 @@ namespace NhanAZ\AdvancedDebuger;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockBurnEvent;
 use pocketmine\event\block\BlockFormEvent;
+use pocketmine\event\block\BlockGrowEvent;
 use pocketmine\event\Event;
 use pocketmine\event\Listener;
 use function end;
@@ -14,7 +15,6 @@ use function explode;
 use function implode;
 // TODO: use pocketmine\event\block\BaseBlockChangeEvent; [abstract class]
 // TODO: use pocketmine\event\block\BlockEvent; [abstract class]
-// TODO: use pocketmine\event\block\BlockGrowEvent;
 // TODO: use pocketmine\event\block\BlockItemPickupEvent;
 // TODO: use pocketmine\event\block\BlockMeltEvent;
 // TODO: use pocketmine\event\block\BlockPlaceEvent;
@@ -180,6 +180,17 @@ class EventListener implements Listener {
 	 * @handleCancelled true
 	 */
 	public function onBlockForm(BlockFormEvent $event) : void {
+		$eventName = $this->getEventName($event);
+		$block = $event->getBlock();
+		$blockPosition = $block->getPosition();
+		$newState = $event->getNewState();
+		$this->getMain()->debug("EventName: $eventName | Block: $block [$blockPosition] | NewState: $newState");
+	}
+
+	/**
+	 * @handleCancelled true
+	 */
+	public function onBlockGrow(BlockGrowEvent $event) : void {
 		$eventName = $this->getEventName($event);
 		$block = $event->getBlock();
 		$blockPosition = $block->getPosition();
